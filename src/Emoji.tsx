fed2b5037a7126c240d1e2e6b37aef0fbcb8676e
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2025-01-06 20:39:52 -0300

    Adiciona botões de troca

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -13,21 +13,23 @@ export default function Emoji(){
     const [situacao, setSituacao] = useState("happy")
 
     function toHappy(){
-        console.log(`Antes: ${situacao}`)
         setSituacao("happy")
-        console.log(`Depois: ${situacao}`)
+    }
+
+    function toSick(){
+        setSituacao("sick")
     }
 
     function toDead(){
-        console.log(`Antes: ${situacao}`)
         setSituacao("dead")
-        console.log(`Depois: ${situacao}`)
     }
+
     return (
     <div className="emoji">
         <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
         <div className="acoes">
             <button onClick={toHappy}>Vivo</button>
+            <button onClick={toSick}>Doente</button>
             <button onClick={toDead}>Morto</button>
         </div>
     </div>)
