58ddd45ae10119dc89884612d88f5da72ff8f884
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2025-01-06 20:30:59 -0300

    Adiciona alteração de estado

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Emoji.css"
 
 const EMOJIS = new Map<string, string>(
@@ -9,6 +10,25 @@ const EMOJIS = new Map<string, string>(
 );
 
 export default function Emoji(){
-    let situacao = "dead";
-    return <div className="emoji">{EMOJIS.get(situacao) || "🫥"}</div>
+    const [situacao, setSituacao] = useState("happy")
+
+    function toHappy(){
+        console.log(`Antes: ${situacao}`)
+        setSituacao("happy")
+        console.log(`Depois: ${situacao}`)
+    }
+
+    function toDead(){
+        console.log(`Antes: ${situacao}`)
+        setSituacao("dead")
+        console.log(`Depois: ${situacao}`)
+    }
+    return (
+    <div className="emoji">
+        <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
+        <div className="acoes">
+            <button onClick={toHappy}>Vivo</button>
+            <button onClick={toDead}>Morto</button>
+        </div>
+    </div>)
 }
