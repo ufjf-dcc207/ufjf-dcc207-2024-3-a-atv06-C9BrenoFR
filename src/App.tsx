1d22475d999e484be113e06bb394623872f2dd18
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-01-09 22:18:49 -0300

    Altera App para receber a barra de emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
 import Emoji from './Emoji'
+import EmojiBar from './EmojiBar'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
     <Emoji/>
-    </>
+    <div className='atributos'>
+      <EmojiBar emoji="💗"/>
+      <EmojiBar emoji="🍕"/>
+      <EmojiBar emoji="💧"/>
+      <EmojiBar emoji="⚡"/>
+    </div>
+    </div>
   )
 }
 
