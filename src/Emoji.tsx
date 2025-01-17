071e544505b9faed70ccc19bfaa03641356e580b
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-01-16 22:13:50 -0300

    Adiciona relação dos atributos e os rostos

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -6,6 +6,7 @@ const EMOJIS = new Map<string, string>(
     [
         ["happy", "😊"],
         ["sick", "🤢"],
+        ["sleepy", "😴"],
         ["dead", "😵"],
     ]
 );
@@ -18,34 +19,69 @@ export default function Emoji(){
     const [agua, setAgua] = useState(3)
     const [luz, setLuz] = useState(true)
 
+    function changeFace(s = saude, e = energia){
+        if(s === 0)
+            setSituacao("dead")
+        else if(s <= 2)
+            setSituacao("sick")
+        else if(e <= 2)
+            setSituacao("sleepy")
+        else
+            setSituacao("happy")
+    }
+
     function onFeed(){
-        setComida(Math.min(comida + 1, 5))
+        if(situacao !== "dead")
+            setComida(Math.min(comida + 1, 5))
     }
 
     function onDrink(){
-        setAgua(Math.min(agua + 1, 5))
+        if(situacao !== "dead")
+            setAgua(Math.min(agua + 1, 5))
     }
 
     function onChangeLight(){
-        setLuz(!luz)
+        if(situacao !== "dead")
+            setLuz(!luz)
     }
 
     function nextState(){
-        setComida(Math.max(0, comida - 1))
-        setAgua(Math.max(0, agua - 1))
-        if(luz)
-            setEnergia(Math.max(0, energia - 1))
-        else
-            setEnergia(Math.min(energia + 1, 5))
+        if(situacao !== "dead"){
+            let e = energia
+            let c = comida
+            let a = agua
+            let s = saude
 
-        if(comida === 0)
-            setSaude(s => Math.max(0, s-1))
-        if(agua === 0)
-            setSaude(s => Math.max(0, s-1))
-        if(energia === 0)
-            setSaude(s => Math.max(0, s-1))
+            if(luz){
+                e = Math.max(0, e - 1)
+            }
+            else{
+                e = Math.min(e + 1, 5)
+                if(c > 0 && a > 0){
+                    s = Math.min(s + 1, 5)
+                }
+            }
 
-        
+            if(c === 0){
+                s = Math.max(0, s-1)
+            }
+            if(a === 0){
+                s = Math.max(0, s-1)
+            }
+            if(e === 0){
+                s = Math.max(0, s-1)
+            }
+
+            c = Math.max(0, c - 1)
+            a = Math.max(0, a - 1)
+
+            setAgua(a)
+            setComida(c)
+            setEnergia(e)
+            setSaude(s)
+
+            changeFace(s, e)
+        }
     }
 
     return (
